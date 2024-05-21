f0fc102a0cfb75f099568dc98b1c9e6bd915aa7a
JitenChapagain <jitenchapagain45@gmail.com>
2024-05-21 13:47:08 +0545

    final change.

--- download/urls.py
+++ download/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
-from .views import *
-
-urlpattern = [
-    path('',index,name='index')
+from django.urls import path 
+from . import views 
+  
+urlpatterns = [ 
+    path('', views.index, name='index'), 
 ]
--- download/views.py
+++ download/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect 
 from pytube import *
-
-# Create your views here.
-def index(request):
-    if request.method == 'POST':
+  
+def index(request): 
+   
+    if request.method == 'POST': 
         
-        link=request.POST['link']   # getting link from frontend
-        video=YouTube(link)
-
-        stream = video.streams.get_lowest_resolution() #setting video resolution
-
-        stream.download()  # downloads video
-
-        return render(request,'index.html')
-
+        link = request.POST['link']  # getting link from frontend 
+        video = YouTube(link) 
+  
+        stream = video.streams.get_lowest_resolution()  # setting video resolution
+          
+        # downloads video 
+        stream.download() 
+  
+        return render(request, 'index.html') 
     return render(request, 'index.html')
