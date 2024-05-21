1f585b6d452c1398f6d85f0488a8a00b72aa9c1e
JitenChapagain <jitenchapagain45@gmail.com>
2024-05-21 13:23:17 +0545

    work

--- download/views.py
+++ download/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from pytube import *
 
 # Create your views here.
+def index(request):
+    if request.method == 'POST':
+        
+        link=request.POST['link']   # getting link from frontend
+        video=YouTube(link)
+
+        stream = video.streams.get_lowest_resolution() #setting video resolution
+
+        stream.download()  # downloads video
+
+        return render(request,'index.html')
+
+    return render(request, 'index.html')
